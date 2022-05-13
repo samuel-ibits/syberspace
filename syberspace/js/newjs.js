6b186a78f20412ed644226148e321270629de15b
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-05-13 20:30:45 +0100

    update
    update

---
+++ syberspace/js/newjs.js
@@ -0,0 +1,13 @@
+function order(){
+    var orderid=document.getElementById('order');
+    // orderid.style.display="block";
+if( orderid.style.display == "none"){
+    orderid.style.display="block";
+}else{
+    orderid.style.display="none";
+}
+// else{
+    // orderid.style.display="none";
+// }
+
+}
